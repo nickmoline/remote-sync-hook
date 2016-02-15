d5a1423b18a87e6f7bbd4c2407e24d5613e57277
Angel Alvarado <angel.alvarado@justia.com>
2016-02-15 17:14:30 -0600

    Adds installer

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Installer for sftp-config-hook 
+
+if ! foobar_loc="$(type -p "jq")" || [ -z "$foobar_loc" ]; then
+  echo "You need to install jq"
+  echo "\tFor Mac: $ brew install jq"
+  echo "\tFor Debian/Ubuntu: $ sudo apt-get install jq"
+  echo "More info: https://stedolan.github.io/jq/download/"
+  exit;
+fi
+
+if [ ! -d ".git" ]; then
+  echo "Not a git repository."
+  exit;
+fi
+
+echo "Downloading script"
+curl -L https://raw.githubusercontent.com/Yelrado/sftp-config-hook/master/post-checkout > .git/hooks/post-checkout
+chmod 755 .git/hooks/post-checkout
+"${EDITOR:-vi}" .git/hooks/post-checkout
+echo "Installed!"
